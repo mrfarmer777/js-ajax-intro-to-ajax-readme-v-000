5eb2ee5f2b0b385816072d2598a15cfab9d3ed0a
Matt Farmer <mrfarmer777@gmail.com>
2018-07-29 04:50:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,13 @@ function getRepositories(){
 }
 
 
-
+function getCommits(el){
+  const name=el.dataset.repo;
+  const req=new XMLHttpRequest();
+  req.addEventListener("load",showCommits);
+  req.open("GET",'https://api.github.com/repos/octocat/'+name+'/commits');
+  req.send();
+}
 
 
 
