c5ad9cf2ee1ed51326e03e9e6306007e0154fef5
Matt Farmer <mrfarmer777@gmail.com>
2018-07-29 04:36:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function showRepositories(event,data){
   console.log(this.responseText);
+  let repoList="<ul>";
+  
+  for (var i=0;i<this.responseText.length; i++){
+    repoList+="<li>"+this.responseText[i]["name"]+"</li>";
+  }
+  repoList+="</ul>";
 }
 
 function getRepositories(){
