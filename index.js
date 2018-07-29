b0df3a7786280097f6f697a926145cc92b1568b8
Matt Farmer <mrfarmer777@gmail.com>
2018-07-29 04:24:03 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function getRepositories(){
+  //req is a new XMLHttpRequest object
+  const req = new XMLHttpRequest();
+  
+  //Construct the request
+  req.open("GET", 'https://api.github.com/users/octocat/repos');
+  
+  
+  //Send the request
+  req.send();
+}
