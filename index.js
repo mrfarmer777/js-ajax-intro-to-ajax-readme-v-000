190fc2e0031a87a4aa26d9015bce8d61cc8898ff
Matt Farmer <mrfarmer777@gmail.com>
2018-07-29 04:53:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -34,6 +34,8 @@ function getCommits(el){
 function showCommits(){
   const commits=JSON.parse(this.responseText);
   const commitsList=`<ul>${commits.map(commit=>'<li><strong>'+commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`;
+  
+  document.getElementById("commits").innerHTML=commitsList;
 }
 
 
